b086625345666d55d02efc3c0a3c431c8d147a0d
VladilavDiD <vladdidyk14@gmail.com>
2026-03-16 12:01:54 +0200

    create a first pack test

--- src/main/java/org/example/Company.java
+++ src/main/java/org/example/Company.java
@@ -5,5 +5,24 @@ public class Company {
     private Company parent;
     private long employeeCount;
 
-    //Getters and Setters
+    public Company(Company parent, long employeeCount) {
+        this.parent = parent;
+        this.employeeCount = employeeCount;
+    }
+
+    public Company getParent() {
+        return parent;
+    }
+
+    public void setParent(Company parent) {
+        this.parent = parent;
+    }
+
+    public long getEmployeeCount() {
+        return employeeCount;
+    }
+
+    public void setEmployeeCount(long employeeCount) {
+        this.employeeCount = employeeCount;
+    }
 }
--- src/test/java/org/example/CompanyServiceImplTest.java
+++ src/test/java/org/example/CompanyServiceImplTest.java
@@ -1,19 +1,55 @@
 package org.example;
 
+import org.junit.jupiter.api.*;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class CompanyServiceImplTest {
 
-    @org.junit.jupiter.api.BeforeEach
+    private final Company main = new Company(null, 2);
+
+    private final Company book = new Company(main, 3);
+
+    private final Company manager = new Company(main, 4);
+
+    private final Company developer = new Company(manager, 8);
+
+    private final Company design = new Company(manager, 6);
+
+    private final Company laver = new Company(null, 1);
+
+    private final ICompanyService companyService = new CompanyServiceImpl();
+
+    @BeforeEach
     void setUp() {
     }
 
-    @org.junit.jupiter.api.AfterEach
+    @AfterEach
     void tearDown() {
     }
 
-    @org.junit.jupiter.api.Test
-    void getTopLevelParent() {
+    @Test
+    void whenCompanyHasNoParentItIsOnTop() {
+        Company result = companyService.getTopLevelParent(main);
+        Assertions.assertEquals(main, result);
+    }
+
+    @Test
+    void whenCompanyIsSingleItIsOnTop() {
+        Company result = companyService.getTopLevelParent(laver);
+        Assertions.assertEquals(laver, result);
+    }
+
+    @Test
+    void whenCompanyHasOneStepToTheTopThenFindTop() {
+        Company result = companyService.getTopLevelParent(book);
+        Assertions.assertEquals(main, result);
+    }
+
+    @Test
+    void whenCompanyHasTwoStepsToTheTopThenFindTop() {
+        Company result = companyService.getTopLevelParent(developer);
+        Assertions.assertEquals(main, result);
     }
 
     @org.junit.jupiter.api.Test
