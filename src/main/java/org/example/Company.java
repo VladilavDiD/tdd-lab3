e75c4edd497b11953b88bf3858e670df1aacf035
VladilavDiD <vladdidyk14@gmail.com>
2026-03-16 10:23:22 +0200

    update, add test, junit5 and create CompanyServiceTest

---
+++ src/main/java/org/example/Company.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Company {
+    //parent for this company, nullable when there are no parent
+    private Company parent;
+    private long employeeCount;
+
+    //Getters and Setters
+}
---
+++ src/test/java/org/example/CompanyServiceImplTest.java
@@ -0,0 +1,22 @@
+package org.example;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CompanyServiceImplTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    void tearDown() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getTopLevelParent() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getEmployeeCountForCompanyAndChildren() {
+    }
+}
